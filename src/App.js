4b5de634c41f279e6026e7357103824430ff0d9c
Kevin <kevingeniard2002@gmail.com>
2023-02-21 23:51:57 +0530

    Added base filter setup

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 
   const options = ['Amount', 'Status'];
   const options_sub = {
-    'amount': ['All', 'Acending', 'Descending'],
+    'amount': ['None', 'Acending', 'Descending'],
     'status': ['All', 'Active', 'Inactive']
   };
 
@@ -27,24 +27,33 @@ function App() {
 
 function DropDown(props) { // dropdown component
 
-  const [selected, setSelected] = useState(-1);
-  const [afilter, setAfilter] = useState('All');
-  const [sfilter, setSfilter] = useState('All');
+  const dflt = { 'selected': -1, 'afilter': 'None', 'sfilter': 'All', 'placeHolder': 'None' }; // selected , amount, status, placeholder
+  const [selections, setSelections] = useState(dflt);
+  const { selected, afilter, sfilter, placeHolder } = selections;
 
-  const onSelect = (index) => setSelected(index);
+  const onSelect = (index) => setSelections({ ...selections, "selected": index });
 
-  const subItemSelect = (index) => {
-    console.log(`Selected item: ${index}`);
-    setSelected(-1);
+  const subItemSelect = (label, index) => {
+    const naf = label === 'amount' ? props.options_sub[label][index] : afilter;
+    const nsf = label === 'status' ? props.options_sub[label][index] : sfilter;
+    setSelections(prevState => ({
+      ...prevState,
+      selected: -1,
+      afilter: naf,
+      sfilter: nsf,
+      placeHolder: (nsf === 'All' && naf === 'None') ? 'None' : 'Applied'
+    }));
+    console.log(selections);
     // TODO: update the table view
   };
 
+
   const subSelect = (index) => { // sub menu
-    console.log(index);
+    // console.log(index);
     return <div className='dropdown-sub'>
       {
-        (props.options_sub[`${index.label}`.toLowerCase()]).map((item, index) => {
-          return <span className='dropdown-sub-item' key={index} onClick={() => subItemSelect(index)}>
+        (props.options_sub[`${index.label}`.toLowerCase()]).map((item, ind) => {
+          return <span className='dropdown-sub-item' key={ind} onClick={() => subItemSelect(`${index.label}`.toLowerCase(), ind)}>
             {item}
           </span>
         })
@@ -54,7 +63,7 @@ function DropDown(props) { // dropdown component
 
   return ( // main menu
     <div id="dropdown">
-      <ReactDropdown options={props.options} onChange={onSelect} className="dropdown-main-items" controlClassName='dropdown-main-control' menuClassName='dropdown-main-menu' placeholderClassName='dropdown-main-placeholder' />
+      <ReactDropdown options={props.options} onChange={onSelect} value={placeHolder} className="dropdown-main-items" controlClassName='dropdown-main-control' menuClassName='dropdown-main-menu' placeholderClassName='dropdown-main-placeholder' />
       {
         (selected != -1) ? subSelect(selected) : null
       }
@@ -64,15 +73,39 @@ function DropDown(props) { // dropdown component
 
 function TableView(props) { // depends on the number of columns from the api call
   const titles = ["User ID", "Name", "Pay ID", "Status"];
+  const popupDeets = { "showPopUp": false, "data": "" };
   const [data, setData] = useState([]);
+  const [{ showPopUp, popUpData }, setShowPopup] = useState(popupDeets);
+
   useEffect(() => {
-    fetch('https://api.sampleapis.com/baseball/hitsSingleSeason')
+    fetch('https://devapi.pepcorns.com/api/test/getAllUsers')
       .then(response => response.json())
-      .then(json => setData(json));
+      .then(json => setData(json['response']));
   }, []);
+
+
+  const handleUserClick = (userId) => { // handles user id click
+    console.log(userId);
+    fetch(`https://devapi.pepcorns.com/api/test/getUserById/${userId}`)
+      .then(response => response.json())
+      .then(json => {
+        setShowPopup({ showPopUp: true, popUpData: json })
+        console.log(json);
+      }
+      );
+  };
+
+  const handlePayClick = (payId) => { // handles pay id click
+    console.log(payId);
+  };
+
+  const hidePopUp = () => { // hides the popup
+    setShowPopup({ showPopUp: false, popUpData: "" });
+  };
 
   return (
     <div className='table-view' id='table-view-parent'>
+      {(showPopUp) ? <TablePopUp uid={showPopUp} /> : null}
       <table>
         <tr className='table-title-row'>
           {
@@ -89,7 +122,7 @@ function TableView(props) { // depends on the number of columns from the api cal
             return <tr className='table-data-row'>
               {Object.keys(item).map((key, index) => {
                 const classes = 'table-data-row table-data-item ' + ((index === 0 || index == 2) ? ' table-data-item-selectable' : ''); // hover control
-                return <td id={index} className={classes}>{item[key]}</td>
+                return <td id={index} className={classes} onClick={() => (index === 0) ? handleUserClick(item[key]) : (index === 2) ? handlePayClick(item[key]) : null}>{item[key]}</td>
               }).filter((_, index) => index < 4)}
             </tr>
           })) :
@@ -99,11 +132,9 @@ function TableView(props) { // depends on the number of columns from the api cal
   );
 }
 
-{/* function TableItem(props) {
-  return (
-    //TODO
-  );
-} */}
+function TablePopUp(props) { // TODO: popup for table view
+  return <div>Jelllo {props.uid}</div>
+}
 
 
 export default App;
